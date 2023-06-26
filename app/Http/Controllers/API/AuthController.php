350fde0faf7a59f3609d9aeff28ce1756f178f12
NarayanCalidig <narayan.paswan@calidig.com>
2023-06-26 19:46:43 +0530

    jwt configuration

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public $token = true;
+
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
+    }
+
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(),[
+            "name" => "required|string|min:2|max:100",
+            "email" => "required|email|unique:users",
+            "password" => "required|confirmed|min: 8",
+            
+        ]);
+
+        if($validator->fails()){
+            return response()->json($validator->errors(), 400);
+        }
+
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+        $user->save();
+        
+        if ($this->token) {
+            return $this->login($request);
+        }
+      
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if ($token = $this->guard()->attempt($credentials)) {
+            return $this->respondWithToken($token);
+        }
+
+        return response()->json(['error' => 'Unauthorized'], 401);
+    }
+
+    public function me()
+    {
+        return response()->json($this->guard()->user());
+    }
+
+    public function logout()
+    {
+        $this->guard()->logout();
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+
+    public function refresh()
+    {
+        return $this->respondWithToken($this->guard()->refresh());
+    }
+
+    protected function respondWithToken($token)
+    {   //this is getting user details
+        
+        $user = $this->guard()->user();
+        return response()->json([
+            // 'access_token' => $token,
+            'token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => $this->guard()->factory()->getTTL() * 60,
+            'user' => $user
+            
+        ]);
+    }
+
+    public function guard()
+    {
+        return Auth::guard('api');
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -17,6 +18,16 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+    
     protected $fillable = [
         'name',
         'email',
