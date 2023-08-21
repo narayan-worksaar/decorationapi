e66e965ed4bfd3a2c2dfd6f0599c539c15eb9e1b
NarayanCalidig <narayan.paswan@calidig.com>
2023-08-21 20:22:04 +0530

    resize

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -10,15 +10,22 @@ use App\Rules\UniqueMobileNumber;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Mail;
+use Intervention\Image\Facades\Image;
 
 
 class AuthController extends Controller
 {
+    
     public $token = true;
 
     public function __construct()
     {
         $this->middleware('auth:api', ['except' => ['login', 'register']]);
+        ini_set('memory_limit', '512M');
+         ini_set('max_execution_time', '300');
+        ini_set('upload_max_filesize', '10M');
+        ini_set('post_max_size', '10M');
+        
     }
 
    
@@ -54,11 +61,36 @@ class AuthController extends Controller
             }
            
             
-            if($request->hasFile('aadhaar_card')){
+            // if($request->hasFile('aadhaar_card')){
              
-                $aadhaarImage = time()."_aadhaar".".".$request->file('aadhaar_card')->getClientOriginalExtension();
-                $request->file('aadhaar_card')->storeAs('public/images',$aadhaarImage);
-                $user->aadhaar_card = $aadhaarImage;
+            //     $aadhaarImage = time()."_aadhaar".".".$request->file('aadhaar_card')->getClientOriginalExtension();
+            //     $request->file('aadhaar_card')->storeAs('public/images',$aadhaarImage);
+            //     $user->aadhaar_card = $aadhaarImage;
+            // }
+
+            if ($request->hasFile('aadhaar_card')) {
+                $aadhaarImage = time() . "_aadhaar" . "." . $request->file('aadhaar_card')->getClientOriginalExtension();
+                
+              // Resize to a smaller dimension
+            $tempImage = Image::make($request->file('aadhaar_card'));
+            $tempImage->resize(800, null, function ($constraint) {
+                $constraint->aspectRatio();
+                $constraint->upsize();
+            });
+
+            // Resize to the final dimensions
+            $finalImage = clone $tempImage;
+            $finalImage->resize(500, null, function ($constraint) {
+                $constraint->aspectRatio();
+                $constraint->upsize();
+            });
+
+            // Save the final image
+            $finalImage->save(public_path('storage/images/' . $aadhaarImage), 100);
+
+            // Set the user's aadhaar_card to the image filename
+            $user->aadhaar_card = $aadhaarImage;
+
             }
 
             if($request->hasFile('driving_license')){
@@ -83,7 +115,7 @@ class AuthController extends Controller
         // }
       
     }
-
+    /*
     public function login(Request $request)
     {
         $credentials = $request->only('email', 'password');
@@ -91,6 +123,30 @@ class AuthController extends Controller
         if ($token = $this->guard()->attempt($credentials)) {
             // Check if the user's status is "active" before allowing them to login
             $user = $this->guard()->user();
+            if ($user->status === 'active') {
+                return $this->respondWithToken($token);
+            } else {
+                return response()->json(['error' => 'Your account is not active.'], 401);
+            }
+            
+        }
+
+        return response()->json(['error' => 'Unauthorized'], 401);
+    }
+    */
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email_or_mobile', 'password');
+
+         // Modify the query to search for users by email or mobile_number
+            $user = User::where(function ($query) use ($credentials) {
+                $query->where('email', $credentials['email_or_mobile'])
+                    ->orWhere('mobile_number', $credentials['email_or_mobile']);
+            })->first();
+
+        if ($user && $token = $this->guard()->attempt(['email' => $user->email, 'password' => $credentials['password']])) {     
+            
             if ($user->status === 'active') {
                 return $this->respondWithToken($token);
             } else {
